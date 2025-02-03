96b8c4e285d75ad0bd18ffa0e6164ea4937d5f65
Osniel Torres <osniel-torres@hotmail.com>
2025-02-02 22:02:08 -0500

    made update to strapi plugin

--- backend/src/plugins/auto-dealer-fields/admin/src/index.ts
+++ backend/src/plugins/auto-dealer-fields/admin/src/index.ts
@@ -1,24 +1,8 @@
-import { getTranslation } from './utils/getTranslation';
-import { PLUGIN_ID } from './pluginId';
 import { Initializer } from './components/Initializer';
-import { PluginIcon } from './components/PluginIcon';
+import { PLUGIN_ID } from './pluginId';
 
 export default {
   register(app: any) {
-    app.addMenuLink({
-      to: `plugins/${PLUGIN_ID}`,
-      icon: PluginIcon,
-      intlLabel: {
-        id: `${PLUGIN_ID}.plugin.name`,
-        defaultMessage: PLUGIN_ID,
-      },
-      Component: async () => {
-        const { App } = await import('./pages/App');
-
-        return App;
-      },
-    });
-
     app.registerPlugin({
       id: PLUGIN_ID,
       initializer: Initializer,
